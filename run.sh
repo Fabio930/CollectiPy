d699255a44383f7065389287e0d548f8b343d1c0
Fabio Oddi <fabio.oddi@uniroma1.it>
2025-10-27 16:07:09 +0100

    .

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ./.venv
-# python3 ../src/main.py -c ../config/random_wp_test.json
-# python3 ../src/main.py -c ../config/spin_model_test_selection.json
-python3 ../src/main.py -c ../config/spin_model_test_flocking.json
+# python3.10 ../src/main.py -c ../config/random_wp_test.json
+python3.10 ../src/main.py -c ../config/spin_model_test_selection.json
+# python3.10 ../src/main.py -c ../config/spin_model_test_flocking.json
 cd ../
