9281113642d82d254738d3efb4d4ce17fce368b1
Fabio Oddi <fabio.oddi@uniroma1.it>
2025-12-06 19:23:52 +0100

    .

--- src/config.py
+++ src/config.py
@@ -7,6 +7,8 @@
 #  license. Attribution is required if this code is used in other works.
 # ------------------------------------------------------------------------------
 
+from __future__ import annotations
+
 import itertools
 import json
 from typing import Callable
